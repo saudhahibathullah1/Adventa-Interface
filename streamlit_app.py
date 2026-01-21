0c7c8880d6c867dc55b4a34ce142aeec2f613fc8
saudhahibathullah1 <saudha.hibathullah@gmail.com>
2026-01-22 02:45:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Adventa')
 
 st.write('Hello world!')
+
+st.title("AdVanta – Data Import & Cleaning")
+
+uploaded_file = st.file_uploader(
+    "Upload Advertising Dataset (CSV)",
+    type=["csv"]
+)
